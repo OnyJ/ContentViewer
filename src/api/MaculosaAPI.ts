93ec13228b258850ac2c2c0cecb565527aa9221a
Josselin <julien.josselin75@hotmail.fr>
2021-08-06 18:44:18 +0200

    getEPGProgram api function handles url presence

--- src/api/MaculosaAPI.ts
+++ src/api/MaculosaAPI.ts
@@ -1,6 +1,14 @@
-export function getEPGPrograms(channelId: number): Promise<number> {
+export function getEPGPrograms(channelId: number): Promise<void> {
     const URL = `https://lab-gcp.ifeelsmart.net/demo/v1/data/epg/programs?channel=${channelId}`;
     return fetch(URL)
         .then((response) => response.json())
+        .then((response) => {
+            const contents = response.contents.map((content) => {
+                const imageUrl = content.pictures != undefined && content.pictures.thumbnails[0];
+                return { ...content, picture: imageUrl };
+            });
+            return { ...response, contents: contents };
+        })
+        .then((response) => response)
         .catch((error) => console.error(error.messages));
 }
