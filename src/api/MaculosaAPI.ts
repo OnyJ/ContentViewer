d6731ec4f1c3fd7c77c6fd9e2af9b12203e5a43d
Josselin <julien.josselin75@hotmail.fr>
2021-08-06 16:37:16 +0200

    refactor getEPGPrograms api function

--- src/api/MaculosaAPI.ts
+++ src/api/MaculosaAPI.ts
@@ -1,9 +1,6 @@
 export function getEPGPrograms(channelId) {
     const URL = `https://lab-gcp.ifeelsmart.net/demo/v1/data/epg/programs?channel=${channelId}`;
-    return new Promise((resolve) => {
-        fetch(URL)
-            .then((response) => response.json())
-            .then((response) => resolve(response))
-            .catch((error) => console.error(error.messages));
-    });
+    return fetch(URL)
+        .then((response) => response.json())
+        .catch((error) => console.error(error.messages));
 }
