dfb68a89cf11a7a73387ead630c045fc9d3127ce
Josselin <julien.josselin75@hotmail.fr>
2021-08-06 18:48:35 +0200

    add i18n and DisplayProgram container to epg_programs

--- src/containers/epg_programs/index.tsx
+++ src/containers/epg_programs/index.tsx
@@ -1,12 +1,35 @@
 import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { getEPGPrograms } from '../../api/MaculosaAPI';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { saveEPGPrograms } from '../../redux/epg_programs';
 
-export function EPGPrograms({ channelId }): JSX.Element {
+const DisplayProgram = ({ content, t }) => (
+    <div>
+        <p>{content.title}</p>
+        <p>{content.category}</p>
+        <p>
+            <span>
+                {content.broadcast_datetime
+                    .slice(0, 16)
+                    .replace(/-/g, '/')
+                    .replace('T', t('from'))
+                    .replace(':', t('time_separation'))}
+            </span>
+            <span>
+                {t('to')}
+                {content.broadcast_end_datetime.slice(11, 16).replace(':', t('time_separation'))}
+            </span>
+        </p>
+        <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} width="300" />
+    </div>
+);
+
+export function EPGPrograms({ channelId }: { channelId: number }): JSX.Element {
     const programs = useAppSelector((state) => state.EPGPrograms.data);
     const dispatch = useAppDispatch();
+    const { t } = useTranslation();
 
     useEffect(() => {
         const fetchProgram = async () => {
@@ -15,17 +38,24 @@ export function EPGPrograms({ channelId }): JSX.Element {
         fetchProgram();
     }, []);
 
-    // Future component display
-    if (programs.contents !== undefined) console.log(programs.contents);
-    else console.log('No content found');
-
     return (
         <div>
-            <h2>Here are the programs</h2>
+            <h2>{t('epg_title')}</h2>
+            {programs.contents !== undefined ? (
+                Array.isArray(programs.contents) &&
+                programs.contents.map((content, key) => <DisplayProgram content={content} key={key} t={t} />)
+            ) : (
+                <p>{t('no_content')}</p>
+            )}
         </div>
     );
 }
 
 EPGPrograms.propTypes = {
     channelId: PropTypes.number,
+};
+
+DisplayProgram.propTypes = {
+    content: PropTypes.object,
+    t: PropTypes.func,
 };
