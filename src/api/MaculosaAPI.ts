d7c7479ce96faf169b7ce1f5409a9136f55d0c27
Josselin <julien.josselin75@hotmail.fr>
2021-08-06 18:20:16 +0200

    fix getEPGPrograms api function typing

--- src/api/MaculosaAPI.ts
+++ src/api/MaculosaAPI.ts
@@ -1,4 +1,4 @@
-export function getEPGPrograms(channelId) {
+export function getEPGPrograms(channelId: number): Promise<number> {
     const URL = `https://lab-gcp.ifeelsmart.net/demo/v1/data/epg/programs?channel=${channelId}`;
     return fetch(URL)
         .then((response) => response.json())
