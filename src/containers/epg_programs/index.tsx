e3d6e841fc17bddd5778c551d17b3e763dbe3ccf
Josselin <julien.josselin75@hotmail.fr>
2021-08-09 13:05:41 +0200

    include epg_programs container in new styling

--- src/containers/epg_programs/index.tsx
+++ src/containers/epg_programs/index.tsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import '../../styles/epg_programs.scss';
+import '../../styles/index.css';
 
 export const ProgramCard = ({ content, t }) => (
-    <div className={'program-card'}>
+    <div className={'content-card epg-program-card'}>
         <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} />
         <div className={'card-info'}>
             <p className="card-title">{content.title}</p>
-            <p>{content.category}</p>
             <p>
+                {content.category}
+                <br />
                 <span>
                     {content.broadcast_datetime
                         .slice(0, 16)
