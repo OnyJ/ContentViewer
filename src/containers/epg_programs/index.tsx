70b13a60f93303397a5625b9a0b5dd3d3d1dc1e4
Josselin <julien.josselin75@hotmail.fr>
2021-08-09 08:45:03 +0200

    rename DisplayProgram container into ProgramCard

--- src/containers/epg_programs/index.tsx
+++ src/containers/epg_programs/index.tsx
@@ -1,61 +1,31 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { getEPGPrograms } from '../../api/MaculosaAPI';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { saveEPGPrograms } from '../../redux/epg_programs';
+import React from 'react';
+import '../../styles/epg_programs.scss';
 
-const DisplayProgram = ({ content, t }) => (
-    <div>
-        <p>{content.title}</p>
-        <p>{content.category}</p>
-        <p>
-            <span>
-                {content.broadcast_datetime
-                    .slice(0, 16)
-                    .replace(/-/g, '/')
-                    .replace('T', t('from'))
-                    .replace(':', t('time_separation'))}
-            </span>
-            <span>
-                {t('to')}
-                {content.broadcast_end_datetime.slice(11, 16).replace(':', t('time_separation'))}
-            </span>
-        </p>
-        <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} width="300" />
+export const ProgramCard = ({ content, t }) => (
+    <div className={'program-card'}>
+        <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} />
+        <div className={'card-info'}>
+            <p className="card-title">{content.title}</p>
+            <p>{content.category}</p>
+            <p>
+                <span>
+                    {content.broadcast_datetime
+                        .slice(0, 16)
+                        .replace(/-/g, '/')
+                        .replace('T', t('from'))
+                        .replace(':', t('time_separation'))}
+                </span>
+                <span>
+                    {t('to')}
+                    {content.broadcast_end_datetime.slice(11, 16).replace(':', t('time_separation'))}
+                </span>
+            </p>
+        </div>
     </div>
 );
 
-export function EPGPrograms({ channelId }: { channelId: number }): JSX.Element {
-    const programs = useAppSelector((state) => state.EPGPrograms.data);
-    const dispatch = useAppDispatch();
-    const { t } = useTranslation();
-
-    useEffect(() => {
-        const fetchProgram = async () => {
-            dispatch(saveEPGPrograms(await getEPGPrograms(channelId)));
-        };
-        fetchProgram();
-    }, []);
-
-    return (
-        <div>
-            <h2>{t('epg_title')}</h2>
-            {programs.contents !== undefined ? (
-                Array.isArray(programs.contents) &&
-                programs.contents.map((content, key) => <DisplayProgram content={content} key={key} t={t} />)
-            ) : (
-                <p>{t('no_content')}</p>
-            )}
-        </div>
-    );
-}
-
-EPGPrograms.propTypes = {
-    channelId: PropTypes.number,
-};
-
-DisplayProgram.propTypes = {
+ProgramCard.propTypes = {
     content: PropTypes.object,
     t: PropTypes.func,
 };
