7defdafa4898c8a3d1a483fb2cbfde4d1ed00029
Josselin <julien.josselin75@hotmail.fr>
2021-08-09 16:16:37 +0200

    VOD ContentCard handles states for modal

--- src/containers/vod_content/index.tsx
+++ src/containers/vod_content/index.tsx
@@ -1,29 +1,49 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setShowModal } from '../../redux/modal';
 import '../../styles/index.css';
 import '../../styles/vod_content.scss';
 
-export const ContentCard = ({ content, t }) => (
-    <div className={'content-card vod-content-card'}>
-        <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} />
-        <div className={'card-info'}>
-            <p>
-                <span className="card-title">{content.title}</span>
-                <br />
-                <span>{` (${content.genre.replace(',', ', ')})`}</span>
-                <span>{t('on') + content.content_provider}</span>
-            </p>
-            <p>
-                <span>{content.directors != '' && t('by') + content.directors + ' '}</span>
-            </p>
-            {/* Display details
-            <p>{'Cast: ' + content.cast}</p>
-            <p>{content.short_summary}</p>
-            <p>{content.summary}</p>
-            */}
+export const ContentCard = ({ content, t }) => {
+    const modalState = useAppSelector((state) => state.Modal);
+    const dispatch = useAppDispatch();
+
+    function showModal() {
+        if (modalState.show) {
+            dispatch(
+                setShowModal({
+                    show: false,
+                    content: {},
+                }),
+            );
+        } else {
+            dispatch(
+                setShowModal({
+                    show: true,
+                    content: content,
+                }),
+            );
+        }
+    }
+
+    return (
+        <div className={'content-card vod-content-card'} onClick={() => showModal()}>
+            <img src={`${content.picture}`} alt={t('epg_img_alt') + content.title} />
+            <div className={'card-info'}>
+                <p>
+                    <span className="card-title">{content.title}</span>
+                    <br />
+                    <span>{` (${content.genre.replace(',', ', ')})`}</span>
+                    <span>{t('on') + content.content_provider}</span>
+                </p>
+                <p>
+                    <span>{content.directors != '' && t('by') + content.directors + ' '}</span>
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 ContentCard.propTypes = {
     content: PropTypes.object,
