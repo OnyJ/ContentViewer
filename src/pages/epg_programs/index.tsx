487eade437a66c111e39c9573f324764e9d5b6e0
Josselin <julien.josselin75@hotmail.fr>
2021-08-09 08:43:46 +0200

    isolate epg_programes page from container

---
+++ src/pages/epg_programs/index.tsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { getEPGPrograms } from '../../api/MaculosaAPI';
+import { ProgramCard } from '../../containers/epg_programs';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { saveEPGPrograms } from '../../redux/epg_programs';
+
+export default function EPGPrograms(): JSX.Element {
+    const channelId = 444;
+    const programs = useAppSelector((state) => state.EPGPrograms.data);
+    const dispatch = useAppDispatch();
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        const fetchProgram = async () => {
+            dispatch(saveEPGPrograms(await getEPGPrograms(channelId)));
+        };
+        fetchProgram();
+    }, []);
+
+    return (
+        <div>
+            <h2>{t('epg_title')}</h2>
+            {programs.contents !== undefined ? (
+                Array.isArray(programs.contents) &&
+                programs.contents.map((content, key) => <ProgramCard content={content} key={key} t={t} />)
+            ) : (
+                <p>{t('no_content')}</p>
+            )}
+        </div>
+    );
+}
+
+EPGPrograms.propTypes = {
+    channelId: PropTypes.number,
+};
